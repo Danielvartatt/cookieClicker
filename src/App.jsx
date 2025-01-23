933e5e5805b8ac676790b1d9cdd1688d562591e3
Danielvartatt <daniel1.kodehode@gmail.com>
2025-01-23 12:12:21 +0100

    task + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
-import "./App.css";
 import { useState } from "react";
+import styles from "./assets/Cookie.module.css";
+import "./assets/cookie.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,9 +10,18 @@ function App() {
     document.title = `Cookie count is ${count}`;
   }, [count]);
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>Cookie</button>
+    <div className={styles.cookiebackground}>
+      <div className={styles.maindiv}>
+        <div className={styles.cookiediv}>
+          <h1 className={styles.cookiebox}>cookie number:{count}</h1>
+          <button
+            onClick={() => setCount((prev) => prev + 1)}
+            className={styles.button}
+          >
+            <img src="./image/cookie.png" className={styles.cookie} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
